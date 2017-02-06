7836d8f64e2ce8b0c93aaa291302616bf85db872
Electrovirus <E.electrovirus@gmail.com>
2017-02-06 18:05:14 +0330

    TB | selfbot

--- plugins/Identification.lua
+++ plugins/Identification.lua
@@ -1,10 +1,7 @@
 do
 function run(msg, matches)
-  if matches[1] == 'userid' and is_sudo(msg) then
+  if matches[1] == 'ids' and is_sudo(msg) then
 return "🔄Your id : "..msg.from.id.."\n━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n🔀Group id∶"..msg.to.id..""
-end
-  if matches[1] == 'gpid' then
-return "🔀Group id : "..msg.to.id..""  
 end
   if matches[1] == 'phone' then
 return "🔁Your phoneNumber : +"..(msg.from.phone or '404 \nI dont have your number')..""
@@ -12,8 +9,7 @@ end
 end
 return {
   usage = {
-    '!userid : user id',
-    '!gpid : group id',
+    '!ids : user id'
     '!phone : phone number'
   },
   patterns = {
