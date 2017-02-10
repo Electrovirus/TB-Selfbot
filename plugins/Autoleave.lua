dec8d5b018fb0855cb2d36ae1d0d6d76d5818c35
Electrovirus <E.electrovirus@gmail.com>
2017-02-10 11:15:08 +0330

    Update Autoleave.lua

--- plugins/Autoleave.lua
+++ plugins/Autoleave.lua
@@ -4,6 +4,7 @@ local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
     if matches[1] == 'left' and is_sudo(msg) then
+send_large_msg(receiver, "🔄Bye everybody Hope to see you soon \nContact me in @Ev_cnt_bot", ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_friend(msg) then
